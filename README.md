f592edc8e3f278423afe44dbee6f318d4866e31f
tmwil <travis.m.wilson@vumc.org>
2026-02-11 10:00:26 -0800

    changed email

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Disabled Module
-This module was intentionaly disabled due to a found security vulnerability and lack of responsible party to adress such issues. If you owuld like to take responsibility for maintaining this module please reach out to datacore@vumc.org.
+This module was intentionaly disabled due to a found security vulnerability and lack of responsible party to adress such issues. If you would like to take responsibility for maintaining this module, and addressing any known security issues please reach out to redcap-external-module-framework@vumc.org.
--- SecurityDisabledModule.php
+++ SecurityDisabledModule.php
@@ -11,7 +11,7 @@ class SecurityDisabledModule extends AbstractExternalModule
 		if ($project_id && PAGE !== "surveys/index.php") {
 			$moduleConfig = $this->getConfig();
 
-			$alertText = 'The \"[DISABLED_MODULE]\" External Module has been disabled</strong> due to a found security vulnerability. Please contact your administartor if this is blocking work. To remove this notice disable the module on this project from the \"External Modules - Project Module Manager\" page.';
+			$alertText = 'The \"[DISABLED_MODULE]\" External Module has been disabled</strong> due to a found security vulnerability. Please contact your administrator if this is blocking work. To remove this notice disable the module on this project from the \"External Modules - Project Module Manager\" page.';
 
 			$alertText = str_replace('[DISABLED_MODULE]', $this->escape($moduleConfig['name']), $alertText);
 
